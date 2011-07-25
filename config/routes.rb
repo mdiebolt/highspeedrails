78e268e0099b198235bc89ec48a56ba5dcad8cef
Matt Diebolt <mdiebolt@gmail.com>
2011-07-24 17:02:04 -0700

    adding nested route for submissions

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,14 @@
 Highspeedrails::Application.routes.draw do
   match 'user/edit' => 'users#edit', :as => :edit_current_user
-
   match 'signup' => 'users#new', :as => :signup
-
   match 'logout' => 'sessions#destroy', :as => :logout
-
   match 'login' => 'sessions#new', :as => :login
 
-  resources :sessions
+  resources :competitions do
+    resources :submissions
+  end
 
-  resources :users
+  resources :sessions, :users
 
   root :to => 'users#new'
 
