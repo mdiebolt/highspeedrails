139a03e341b194c5e79542f5fc77c42046dc2fff
Matt Diebolt <mdiebolt@gmail.com>
2011-07-24 17:02:22 -0700

    adding hack to fix shoulda for rails 3.1

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,6 +2,7 @@ ENV["RAILS_ENV"] = "test"
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 require File.expand_path('../factories', __FILE__)
+require 'shoulda/rails'
 
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.(yml|csv) for all tests in alphabetical order.
